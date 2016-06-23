1a75efbbafea0c249d19345b2d0b8dc236bb9368
CEREBR4L <mattmillergraphics@gmail.com>
2016-06-23 20:30:30 +0100

    to gh-pages and beyond!

---
+++ js/app.js
@@ -0,0 +1,18 @@
+new Vue ({
+	el: '#images',
+	data: {
+		images: {[
+			"../avatars/klingoneCEREBForeheadDoneHD.png",
+			"../avatars/ceb3.0.png",
+			"../avatars/ceb3.1.png",
+			"../avatars/cerebr4l_2000.png",
+			"../avatars/eXMachinaTerminatorSuperModel.png",
+			"../avatars/eyez.png",
+			"../avatars/fresh.png",
+			"../avatars/fresh-badge.png",
+			"../avatars/klingoneCEREBForeheadDone.png",
+			"../avatars/l33tceb2.png",
+			"../avatars/moonbase.png"
+		]}
+	}
+})
